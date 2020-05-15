a2febc77535a08fdce499071fc0c5ea76d84f2c1
Nunna <nnunna@jcp.com>
2020-05-15 02:29:22 -0500

    Update-12

--- JenkinsPipeline/dockerbuild.sh
+++ JenkinsPipeline/dockerbuild.sh
@@ -1,5 +1,5 @@
 
 imagename=$1
-/usr/local/bin/docker build -t $imagename /opt/sample-website/
+/usr/local/bin/docker build -t $imagename /Users/nnunna/cicd-kubectl/
 
 /usr/local/bin/docker tag $imagename nnunna/$imagename:$version
